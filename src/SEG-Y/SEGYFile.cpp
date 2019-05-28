e4c07f4f8f3c5dd121e69bbf0b19356a1e437a03
morten.ofstad <morten.ofstad@bluware.com>
2019-05-28 14:38:57 +0200

    SEG-Y scanning initial commit

---
+++ src/SEG-Y/SEGYFile.cpp
@@ -0,0 +1,226 @@
+/****************************************************************************
+** Copyright 2019 The Open Group
+** Copyright 2019 Bluware, Inc.
+**
+** Licensed under the Apache License, Version 2.0 (the "License");
+** you may not use this file except in compliance with the License.
+** You may obtain a copy of the License at
+**
+**     http://www.apache.org/licenses/LICENSE-2.0
+**
+** Unless required by applicable law or agreed to in writing, software
+** distributed under the License is distributed on an "AS IS" BASIS,
+** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+** See the License for the specific language governing permissions and
+** limitations under the License.
+****************************************************************************/
+
+#include "File.h"
+#include "SEGYFile.h"
+
+#include <iostream>
+#include <algorithm>
+#include <cassert>
+
+using namespace OpenVDS;
+
+static int
+readFieldFromHeader(const char *header, HeaderField const &headerField, Endianness endianness)
+{
+  if(!headerField.defined())
+  {
+    return 0;
+  }
+
+  // NOTE: SEG-Y byte locations start at 1
+  int index = headerField.m_byteLocation - 1;
+
+  auto signed_header   = reinterpret_cast<const signed   char *>(header);
+  auto unsigned_header = reinterpret_cast<const unsigned char *>(header);
+
+  if(headerField.m_fieldWidth == FieldWidth::FourByte)
+  {
+    if(endianness == Endianness::BigEndian)
+    {
+      return (int32_t)(signed_header[index + 0] << 24 | unsigned_header[index + 1] << 16 | unsigned_header[index + 2] << 8 | unsigned_header[index + 3]);
+    }
+    else
+    {
+      assert(endianness == Endianness::LittleEndian);
+      return (int32_t)(signed_header[index + 3] << 24 | unsigned_header[index + 2] << 16 | unsigned_header[index + 1] << 8 | unsigned_header[index + 0]);
+    }
+  }
+  else
+  {
+    assert(headerField.m_fieldWidth == FieldWidth::TwoByte);
+    if(endianness == Endianness::BigEndian)
+    {
+      return (int16_t)(signed_header[index + 0] << 8 | unsigned_header[index + 1]);
+    }
+    else
+    {
+      assert(endianness == Endianness::LittleEndian);
+      return (int16_t)(signed_header[index + 1] << 8 | unsigned_header[index + 0]);
+    }
+  }
+}
+
+bool
+SEGYFileInfo::readTraceHeader(OpenVDS::File const &file, int64_t trace, char (&header)[SEGY::TraceHeaderSize], OpenVDS::IOError &error)
+{
+  int64_t
+    offset = SEGY::TextualFileHeaderSize + SEGY::BinaryFileHeaderSize + trace * m_traceSize;
+
+  return file.read(header, offset, SEGY::TraceHeaderSize, error);
+}
+
+bool
+SEGYFileInfo::scan(OpenVDS::File const &file, HeaderField const &primaryKeyHeaderField)
+{
+  char textualFileHeader[SEGY::TextualFileHeaderSize];
+  char binaryFileHeader[SEGY::BinaryFileHeaderSize];
+  char traceHeader[SEGY::TraceHeaderSize];
+
+  OpenVDS::IOError error;
+
+  file.read(textualFileHeader,                          0, SEGY::TextualFileHeaderSize, error) &&
+  file.read(binaryFileHeader, SEGY::TextualFileHeaderSize, SEGY::BinaryFileHeaderSize,  error);
+
+  if(error.code != 0)
+  {
+    return false;
+  }
+
+  auto dataSampleFormatCode = SEGY::BinaryHeader::DataSampleFormatCode(readFieldFromHeader(binaryFileHeader, SEGY::BinaryHeader::DataSampleFormatCodeHeaderField, m_headerEndianness));
+
+  int formatSize;
+
+  switch(dataSampleFormatCode)
+  {
+  default:
+    return false;
+  case SEGY::BinaryHeader::DataSampleFormatCode::Int8:
+  case SEGY::BinaryHeader::DataSampleFormatCode::UInt8:
+    formatSize = 1; break;
+  case SEGY::BinaryHeader::DataSampleFormatCode::Int16:
+  case SEGY::BinaryHeader::DataSampleFormatCode::UInt16:
+    formatSize = 2; break;
+  case SEGY::BinaryHeader::DataSampleFormatCode::Int24:
+  case SEGY::BinaryHeader::DataSampleFormatCode::UInt24:
+    formatSize = 3; break;
+  case SEGY::BinaryHeader::DataSampleFormatCode::IBMFloat:
+  case SEGY::BinaryHeader::DataSampleFormatCode::Int32:
+  case SEGY::BinaryHeader::DataSampleFormatCode::FixedPoint:
+  case SEGY::BinaryHeader::DataSampleFormatCode::IEEEFloat:
+  case SEGY::BinaryHeader::DataSampleFormatCode::UInt32:
+    formatSize = 4; break;
+  case SEGY::BinaryHeader::DataSampleFormatCode::IEEEDouble:
+  case SEGY::BinaryHeader::DataSampleFormatCode::Int64:
+  case SEGY::BinaryHeader::DataSampleFormatCode::UInt64:
+    formatSize = 8; break;
+  }
+
+  int64_t fileSize = file.size(error);
+
+  if(error.code != 0)
+  {
+    return false;
+  }
+
+  int sampleCount = readFieldFromHeader(binaryFileHeader, SEGY::BinaryHeader::NumSamplesHeaderField, m_headerEndianness);
+  
+  // If the sample count is not set in the binary header we try to read the first trace header and find the sample count there
+  if(sampleCount == 0)
+  {
+    readTraceHeader(file, 0, traceHeader, error);
+    sampleCount = readFieldFromHeader(traceHeader, SEGY::TraceHeader::NumSamplesHeaderField, m_headerEndianness);
+  }
+
+  if(sampleCount == 0)
+  {
+    return false;
+  }
+
+  int64_t traceDataSize = (fileSize - SEGY::TextualFileHeaderSize - SEGY::BinaryFileHeaderSize);
+
+  m_traceSize  = SEGY::TraceHeaderSize + sampleCount * formatSize;
+  m_traceCount = traceDataSize / m_traceSize;
+
+  if(traceDataSize % m_traceSize != 0)
+  {
+    std::cerr << "Warning: File size is inconsistent with trace size";
+  }
+
+  const int64_t lastTrace = (m_traceCount - 1);
+
+  // The inside/outside trace is the last known trace in the current segment and the first known trace outside the current segment (used when binary searching)
+  int64_t insideTrace = 0, outsideTrace = 0, jump = 1;
+
+  SEGYSegmentInfo
+    segmentInfo;
+
+  int64_t trace = 0;
+
+  int nextPrimaryKey = 0;
+
+  int readCount = 0;
+
+  while(insideTrace != lastTrace)
+  {
+    readTraceHeader(file, trace, traceHeader, error);
+    if(error.code != 0)
+    {
+      return false;
+    }
+    readCount++;
+
+    int primaryKey = readFieldFromHeader(traceHeader, primaryKeyHeaderField, m_headerEndianness);
+
+    if(trace == 0) // start the first segment of the file
+    {
+      segmentInfo = SEGYSegmentInfo(primaryKey, trace);
+    }
+    else if(primaryKey == segmentInfo.m_primaryKey) // expand current segment if the primary key matches
+    {
+      assert(insideTrace == 0 || trace > insideTrace);
+      insideTrace = trace;
+      assert(trace > segmentInfo.m_traceStop);
+      segmentInfo.m_traceStop = trace;
+    }
+    else
+    {
+      assert(outsideTrace == 0 || trace < outsideTrace);
+      outsideTrace = trace;
+      nextPrimaryKey = primaryKey;
+    }
+
+    if(outsideTrace == insideTrace + 1) // current segment is finished
+    {
+      m_segmentInfo.push_back(segmentInfo);
+      int64_t segmentLength = segmentInfo.m_traceStop - segmentInfo.m_traceStart + 1;
+
+      // start a new segment
+      segmentInfo = SEGYSegmentInfo(nextPrimaryKey, outsideTrace);
+      trace = std::min(lastTrace, outsideTrace + segmentLength);
+      insideTrace = 0, outsideTrace = 0, jump = 1;
+    }
+    else if(outsideTrace == 0) // looking for a trace outside the current segment
+    {
+      trace = std::min(lastTrace, trace + jump);
+      jump *= 2;
+    }
+    else if(insideTrace == 0) // looking for a trace inside the current segment
+    {
+      trace = std::max(segmentInfo.m_traceStart, trace - jump);
+      jump *= 2;
+    }
+    else // search for end of segment which must lie somewhere between the last known trace in the current segment and the first known trace outside the current segment
+    {
+      trace = (insideTrace + outsideTrace + 1) / 2;
+    }
+  }
+
+  // final segment is finished
+  m_segmentInfo.push_back(segmentInfo);
+  return true;
+}
---
+++ src/SEG-Y/SEGYFile.h
@@ -0,0 +1,223 @@
+/****************************************************************************
+** Copyright 2019 The Open Group
+** Copyright 2019 Bluware, Inc.
+**
+** Licensed under the Apache License, Version 2.0 (the "License");
+** you may not use this file except in compliance with the License.
+** You may obtain a copy of the License at
+**
+**     http://www.apache.org/licenses/LICENSE-2.0
+**
+** Unless required by applicable law or agreed to in writing, software
+** distributed under the License is distributed on an "AS IS" BASIS,
+** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+** See the License for the specific language governing permissions and
+** limitations under the License.
+****************************************************************************/
+
+#include "File.h"
+
+#include <cstdint>
+#include <vector>
+
+enum class FieldWidth
+{
+  TwoByte,
+  FourByte
+};
+
+enum class Endianness
+{
+  BigEndian,
+  LittleEndian
+};
+
+struct HeaderField
+{
+  int        m_byteLocation;
+  FieldWidth m_fieldWidth;
+
+  HeaderField() : m_byteLocation(), m_fieldWidth() {}
+  HeaderField(int byteLocation, FieldWidth fieldWidth) : m_byteLocation(byteLocation), m_fieldWidth(fieldWidth) {}
+
+  bool       defined() const { return m_byteLocation != 0; }
+};
+
+namespace SEGY
+{
+enum
+{
+  TextualFileHeaderColumns = 80,
+  TextualFileHeaderLines = 40,
+  TextualFileHeaderSize = TextualFileHeaderColumns * TextualFileHeaderLines,
+  BinaryFileHeaderSize = 400,
+  TraceHeaderSize = 240
+};
+
+namespace BinaryHeader
+{
+enum class DataSampleFormatCode
+{
+  Unknown    =  0,
+  IBMFloat   =  1, // 4-byte IBM floating-point
+  Int32      =  2, // 4-byte, two's complement integer
+  Int16      =  3, // 2-byte, two's complement integer
+  FixedPoint =  4, // 4-byte fixed-point with gain (obsolete)
+  IEEEFloat  =  5, // 4-byte IEEE floating-point
+  IEEEDouble =  6, // 8-byte IEEE floating-point
+  Int24      =  7, // 3-byte, two's complement integer
+  Int8       =  8, // 1-byte, two's complement integer
+  Int64      =  9, // 8-byte, two's complement integer
+  UInt32     = 10, // 4-byte, unsigned integer
+  UInt16     = 11, // 2-byte, unsigned integer
+  UInt64     = 12, // 8-byte, unsigned integer
+  UInt24     = 15, // 3-byte, unsigned integer
+  UInt8      = 16  // 1-byte, unsigned integer
+};
+
+enum class SortCode
+{
+  Other                          = -1, // Other (should be explained in a user Extended Textual File Header stanza)
+  Unknown                        =  0, // Unknown
+  As_Recorded                    =  1, // As recorded (no sorting)
+  CDP_Ensemble                   =  2, // CDP ensemble
+  Single_Fold_Continuous_Profile =  3, // Single fold continuous profile
+  Horizontally_Stacked           =  4, // Horizontally stacked
+  Common_Source_Point            =  5, // Common source point
+  Common_Receiver_Point          =  6, // Common receiver point
+  Common_Offset_Point            =  7, // Common offset point
+  Common_Mid_Point               =  8, // Common mid-point
+  Common_Conversion_Point        =  9  // Common conversion point
+};
+
+enum class MeasurementSystem
+{
+  Unknown  = 0,
+  Meters   = 1,
+  Feet     = 2
+};
+
+// Standard header fields
+static HeaderField TracesPerEnsembleHeaderField(13, FieldWidth::TwoByte);
+static HeaderField AuxiliaryTracesPerEnsembleHeaderField(15, FieldWidth::TwoByte);
+static HeaderField SampleIntervalHeaderField(17, FieldWidth::TwoByte);
+static HeaderField NumSamplesHeaderField(21, FieldWidth::TwoByte);
+static HeaderField DataSampleFormatCodeHeaderField(25, FieldWidth::TwoByte);
+static HeaderField EnsembleFoldHeaderField(27, FieldWidth::TwoByte);
+static HeaderField TraceSortingCodeHeaderField(29, FieldWidth::TwoByte);
+static HeaderField MeasurementSystemHeaderField(55, FieldWidth::TwoByte);
+static HeaderField SEGYFormatRevisionNumberHeaderField(301, FieldWidth::TwoByte);
+static HeaderField FixedLengthTraceFlagHeaderField(303, FieldWidth::TwoByte);
+static HeaderField ExtendedTextualFileHeaderCountHeaderField(305, FieldWidth::TwoByte);
+
+} // end namespace BinaryHeader
+
+namespace TraceHeader
+{
+enum class TraceIdentificationCode
+{
+Other                                                  = -1, // Other
+Unknown                                                =  0, // Unknown
+TimeDomainSeismicData                                  =  1, // Time domain seismic data
+Dead                                                   =  2, // Dead
+Dummy                                                  =  3, // Dummy
+TimeBreak                                              =  4, // Time break
+Uphole                                                 =  5, // Uphole
+Sweep                                                  =  6, // Sweep
+Timing                                                 =  7, // Timing
+Waterbreak                                             =  8, // Waterbreak
+NearFieldGunSignature                                  =  9, // Near-field gun signature
+FarFieldGunSignature                                   = 10, // Far-field gun signature
+SeismicPressureSensor                                  = 11, // Seismic pressure sensor
+MulticomponentSeismicSensor_VerticalComponent          = 12, // Multicomponent seismic sensor – Vertical component
+MulticomponentSeismicSensor_CrosslineComponent         = 13, // Multicomponent seismic sensor – Cross-line component
+MulticomponentSeismicSensor_InlineComponent            = 14, // Multicomponent seismic sensor – In-line component
+RotatedMulticomponentSeismicSensor_VerticalComponent   = 15, // Rotated multicomponent seismic sensor – Vertical component
+RotatedMulticomponentSeismicSensor_TransverseComponent = 16, // Rotated multicomponent seismic sensor – Transverse component
+RotatedMulticomponentSeismicSensor_RadialComponent     = 17, // Rotated multicomponent seismic sensor – Radial component
+VibratorReactionMass                                   = 18, // Vibrator reaction mass
+VibratorBaseplate                                      = 19, // Vibrator baseplate
+VibratorEstimatedGroundForce                           = 20, // Vibrator estimated ground force
+VibratorReference                                      = 21, // Vibrator reference
+TimeVelocityPairs                                      = 22, // Time-velocity pairs
+TimeDepthPairs                                         = 23, // Time-depth pairs
+DepthVelocityPairs                                     = 24, // Depth-velocity pairs
+DepthDomainSeismicData                                 = 25, // Depth domain seismic data
+GravityPotential                                       = 26, // Gravity potential
+ElectricField_VerticalComponent                        = 27, // Electric field – Vertical component
+ElectricField_CrosslineComponent                       = 28, // Electric field – Cross-line component
+ElectricField_InlineComponent                          = 29, // Electric field – In-line component
+RotatedElectricField_VerticalComponent                 = 30, // Rotated electric field – Vertical component
+RotatedElectricField_TransverseComponent               = 31, // Rotated electric field – Transverse component
+RotatedElectricField_RadialComponent                   = 32, // Rotated electric field – Radial component
+MagneticField_VerticalComponent                        = 33, // Magnetic field – Vertical component
+MagneticField_CrosslineComponent                       = 34, // Magnetic field – Cross-line component
+MagneticField_InlineComponent                          = 35, // Magnetic field – In-line component
+RotatedMagneticField_VerticalComponent                 = 36, // Rotated magnetic field – Vertical component
+RotatedMagneticField_TransverseComponent               = 37, // Rotated magnetic field – Transverse component
+RotatedMagneticField_RadialComponent                   = 38, // Rotated magnetic field – Radial component
+RotationalSensor_Pitch                                 = 39, // Rotational sensor – Pitch
+RotationalSensor_Roll                                  = 40, // Rotational sensor – Roll
+RotationalSensor_Yaw                                   = 41  // Rotational sensor – Yaw
+};
+
+enum class CoordinateUnits
+{
+  Unknown               = 0,
+  Length                = 1, // Length (meters or feet as specified in Binary File Header bytes 3255-3256 and in Extended Textual Header if Location Data are included in the file)
+  ArcSeconds            = 2, // Seconds of arc (deprecated)
+  DecimalDegrees        = 3, // Decimal degrees (preferred degree representation)
+  DegreesMinutesSeconds = 4  // Degrees, minutes, seconds (DMS)
+};
+
+static HeaderField TraceSequenceNumberHeaderField(1, FieldWidth::FourByte);
+static HeaderField TraceSequenceNumberWithinFileHeaderField(5, FieldWidth::FourByte);
+static HeaderField EnergySourcePointNumberHeaderField(17, FieldWidth::FourByte);
+static HeaderField EnsembleNumberHeaderField(21, FieldWidth::FourByte);
+static HeaderField TraceNumberWithinEnsembleHeaderField(25, FieldWidth::FourByte);
+static HeaderField TraceIdentificationCodeHeaderField(29, FieldWidth::TwoByte);
+static HeaderField CoordinateScaleHeaderField(71, FieldWidth::TwoByte);
+static HeaderField SourceXCoordinateHeaderField(73, FieldWidth::FourByte);
+static HeaderField SourceYCoordinateHeaderField(77, FieldWidth::FourByte);
+static HeaderField GroupXCoordinateHeaderField(81, FieldWidth::FourByte);
+static HeaderField GroupYCoordinateHeaderField(84, FieldWidth::FourByte);
+static HeaderField CoordinateUnitsHeaderField(89, FieldWidth::TwoByte);
+static HeaderField StartTimeHeaderField(109, FieldWidth::TwoByte);
+static HeaderField NumSamplesHeaderField(115, FieldWidth::TwoByte);
+static HeaderField SampleIntervalHeaderField(117, FieldWidth::TwoByte);
+static HeaderField EnsembleXCoordinateHeaderField(181, FieldWidth::FourByte);
+static HeaderField EnsembleYCoordinateHeaderField(185, FieldWidth::FourByte);
+static HeaderField InlineNumberHeaderField(189, FieldWidth::FourByte);
+static HeaderField CrosslineNumberHeaderField(193, FieldWidth::FourByte);
+
+} // end namespace TraceHeader
+
+} // end namespace SEGY
+
+struct SEGYSegmentInfo
+{
+  int     m_primaryKey;
+
+  int64_t m_traceStart,
+          m_traceStop;
+
+  SEGYSegmentInfo() : m_primaryKey(), m_traceStart(), m_traceStop() {}
+  SEGYSegmentInfo(int primaryKey, int64_t trace) : m_primaryKey(primaryKey), m_traceStart(trace), m_traceStop(trace) {}
+};
+
+class SEGYFileInfo
+{
+  int           m_traceSize;
+  int64_t       m_traceCount;
+
+  Endianness    m_headerEndianness;
+
+  std::vector<SEGYSegmentInfo>
+                m_segmentInfo;
+
+public:
+  SEGYFileInfo() : m_traceSize(), m_traceCount(), m_headerEndianness(), m_segmentInfo() {}
+
+  bool readTraceHeader(OpenVDS::File const &file, int64_t trace, char (&header)[SEGY::TraceHeaderSize], OpenVDS::IOError &error);
+  bool scan(OpenVDS::File const &file, HeaderField const &primaryKeyHeaderField);
+};
