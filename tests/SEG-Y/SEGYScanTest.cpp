46820fafdc45582eac6d6c498907f81f9d20ef5e
morten.ofstad <morten.ofstad@bluware.com>
2019-06-11 12:10:46 +0200

    Added SEG-Y scanning test, uses CMAKE define for test data path.

--- tests/SEG-Y/SEGYScanTest.cpp
+++ tests/SEG-Y/SEGYScanTest.cpp
@@ -22,4 +22,44 @@
 
 TEST(SEGYScanTest, scan)
 {
+  OpenVDS::File
+    file;
+
+  OpenVDS::IOError
+    error;
+
+  std::string
+    fileName = TEST_DATA_PATH R"(\SEGY\TeapotDome\filt_mig.sgy)";
+
+  if(!OpenVDS::File::exists(fileName))
+  {
+#ifdef GTEST_SKIP
+    GTEST_SKIP();
+#else
+    return;
+#endif|
+  }
+
+  file.open(fileName, false, false, false, error);
+  EXPECT_EQ(error.code, 0);
+
+  SEGYFileInfo
+    fileInfo;
+
+  bool success = fileInfo.scan(file, HeaderField(189, FieldWidth::FourByte));
+  EXPECT_TRUE(success);
+
+
+  EXPECT_TRUE(fileInfo.m_segmentInfo.front().m_traceStart == 0);
+
+  // check that the segments cover all traces of the file (with no overlap)
+  for(int i = 1; i < fileInfo.m_segmentInfo.size(); i++)
+  {
+    if(fileInfo.m_segmentInfo[i].m_traceStart != fileInfo.m_segmentInfo[i - 1].m_traceStop + 1)
+    {
+      FAIL() << "Segment " << i << " starting at trace " << fileInfo.m_segmentInfo[i].m_traceStart << " does not directly follow segment " << (i - 1) << " ending at trace " << fileInfo.m_segmentInfo[i - 1].m_traceStop;
+    }
+  }
+
+  EXPECT_TRUE(fileInfo.m_segmentInfo.back().m_traceStop == fileInfo.m_traceCount - 1);
 }
