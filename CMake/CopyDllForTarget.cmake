6e33f010d57308770cbe4f6a064c5be3cbaee41b
Jørgen Lind <jorgen.lind@bluware.com>
2019-10-09 13:33:48 +0200

    Make CopyDLL allways run

--- CMake/CopyDllForTarget.cmake
+++ CMake/CopyDllForTarget.cmake
@@ -1,10 +1,13 @@
 function(copyDllForTarget target)
     if (WIN32)
-      add_custom_command(TARGET ${target}
-        POST_BUILD
+      add_custom_command(OUTPUT "${target}_copy_vds"
         COMMAND ${CMAKE_COMMAND} -E copy_if_different $<TARGET_FILE:openvds> $<TARGET_FILE_DIR:${target}>
         DEPENDS openvds
       )
+      set_property(SOURCE "${target}_copy_vds"
+                   PROPERTY SYMBOLIC ON
+                  )
+      target_sources(${target} PRIVATE ${target}_copy_vds)
 
       get_property(runtime_release GLOBAL PROPERTY RUNTIME_LIBS_RELEASE)
       get_property(runtime_debug   GLOBAL PROPERTY RUNTIME_LIBS_DEBUG)
