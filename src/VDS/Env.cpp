5ea88822df91bafbae8a34255c99bd37e26f82ad
Jørgen Lind <jorgen.lind@bluware.com>
2020-02-12 15:17:56 +0100

    Move getting boolean environment variables into accessible function

---
+++ src/VDS/Env.cpp
@@ -0,0 +1,23 @@
+#include "Env.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+namespace OpenVDS
+{
+bool getBooleanEnvironmentVariable(const char *name)
+{
+  const char *var = getenv(name);
+  if (!var)
+    return false;
+  if (strcmp(var, "") == 0)
+    return false;
+  if (strcmp(var, "false") == 0)
+    return false;
+  if (strcmp(var, "FALSE") == 0)
+    return false;
+  if (strcmp(var, "0") == 0)
+    return false;
+  return true;
+}
+}
