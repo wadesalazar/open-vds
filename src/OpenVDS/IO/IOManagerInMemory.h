6d59c96a3fc48d5e9e7b3ac455c2ff0f8ab6796d
morten.ofstad <morten.ofstad@bluware.com>
2020-04-17 16:57:12 +0200

    Make 'Object' in IOManagerInMemory an inner class to avoid name clashes.

--- src/OpenVDS/IO/IOManagerInMemory.h
+++ src/OpenVDS/IO/IOManagerInMemory.h
@@ -29,25 +29,25 @@
 
 namespace OpenVDS
 {
-  struct Object
-  {
-    std::vector<std::pair<std::string, std::string>> metaHeader;
-    std::vector<uint8_t> data;
-  };
-
   class IOManagerInMemory : public IOManager
   {
-    public:
-      IOManagerInMemory(const InMemoryOpenOptions &openOptions, Error &error);
-      ~IOManagerInMemory() override;
+    struct Object
+    {
+      std::vector<std::pair<std::string, std::string>> metaHeader;
+      std::vector<uint8_t> data;
+    };
 
-      std::shared_ptr<Request> ReadObjectInfo(const std::string &objectName, std::shared_ptr<TransferDownloadHandler> handler) override;
-      std::shared_ptr<Request> ReadObject(const std::string &objectName, std::shared_ptr<TransferDownloadHandler> handler, const IORange& range = IORange()) override;
-      std::shared_ptr<Request> WriteObject(const std::string &objectName, const std::string& contentDispostionFilename, const std::string& contentType, const std::vector<std::pair<std::string, std::string>>& metadataHeader, std::shared_ptr<std::vector<uint8_t>> data, std::function<void(const Request & request, const Error & error)> completedCallback = nullptr) override;
-    private:
-      std::mutex m_mutex;
-      std::unordered_map<std::string, Object> m_data;
-      ThreadPool m_threadPool;
+  public:
+    IOManagerInMemory(const InMemoryOpenOptions &openOptions, Error &error);
+    ~IOManagerInMemory() override;
+
+    std::shared_ptr<Request> ReadObjectInfo(const std::string &objectName, std::shared_ptr<TransferDownloadHandler> handler) override;
+    std::shared_ptr<Request> ReadObject(const std::string &objectName, std::shared_ptr<TransferDownloadHandler> handler, const IORange& range = IORange()) override;
+    std::shared_ptr<Request> WriteObject(const std::string &objectName, const std::string& contentDispostionFilename, const std::string& contentType, const std::vector<std::pair<std::string, std::string>>& metadataHeader, std::shared_ptr<std::vector<uint8_t>> data, std::function<void(const Request & request, const Error & error)> completedCallback = nullptr) override;
+  private:
+    std::mutex m_mutex;
+    std::unordered_map<std::string, Object> m_data;
+    ThreadPool m_threadPool;
   };
 }
 
