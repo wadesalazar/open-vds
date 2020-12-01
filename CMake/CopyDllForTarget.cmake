39e35482bba5820e31a1efea96352e492e655681
Jørgen Lind <jorgen.lind@bluware.com>
2020-12-01 10:52:24 +0100

    Use CMAKE_SOURCE_DIR and not PROJECT_SOURCE_DIR

--- CMake/CopyDllForTarget.cmake
+++ CMake/CopyDllForTarget.cmake
@@ -34,12 +34,12 @@ function(copyDllForTarget target)
       foreach(file ${runtime_release})
         add_custom_command(TARGET ${target}
           POST_BUILD
-          COMMAND ${CMAKE_COMMAND} -E copy_if_different $<$<NOT:$<CONFIG:Debug>>:${file}> ${PROJECT_SOURCE_DIR}/README.md $<TARGET_FILE_DIR:${target}>)
+          COMMAND ${CMAKE_COMMAND} -E copy_if_different $<$<NOT:$<CONFIG:Debug>>:${file}> ${CMAKE_SOURCE_DIR}/README.md $<TARGET_FILE_DIR:${target}>)
       endforeach()
       foreach(file ${runtime_debug})
         add_custom_command(TARGET ${target}
           POST_BUILD
-          COMMAND ${CMAKE_COMMAND} -E copy_if_different $<$<CONFIG:Debug>:${file}> ${PROJECT_SOURCE_DIR}/README.md $<TARGET_FILE_DIR:${target}>)
+          COMMAND ${CMAKE_COMMAND} -E copy_if_different $<$<CONFIG:Debug>:${file}> ${CMAKE_SOURCE_DIR}/README.md $<TARGET_FILE_DIR:${target}>)
       endforeach()
     endif()
 endfunction()
