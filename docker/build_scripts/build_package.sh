80f591a4e5bae95bc2b5284fb55d58884c4a8537
Jørgen Lind <jorgen.lind@bluware.com>
2021-03-17 10:44:36 +0100

    Build package update 2
    enable building of multiple python version and adding attributions file.
    Add rpath for tools to point to lib folder.

--- docker/build_scripts/build_package.sh
+++ docker/build_scripts/build_package.sh
@@ -1,9 +1,7 @@
 set -e -u
-base_dir=$(dirname $BASH_SOURCE)
+base_dir=$(realpath $(dirname $BASH_SOURCE))
 
-skbuild_platform=""
-python_executable=""
-openvds_path="$base_dir/../.."
+openvds_path=$(realpath "$base_dir/../..")
 cmake_args="-DBUILD_TESTS=OFF"
 openvds_version=""
 name="openvds"
@@ -17,19 +15,16 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   platform_name="linux"
   skplat_name="linux-x86_64"
   toolset=""
-  python_executable="python"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   cmake_generator="Ninja"
   platform_name="mac"
   skplat_name="maxosx-10.6-x86_64"
   toolset=""
-  python_executable="python"
 elif [[ "$OSTYPE" == "msys" ]]; then
   cmake_generator="Visual Studio 16 2019"
   platform_name="win"
   skplat_name="win-amd64"
   toolset="-Tv140"
-  python_executable="python.exe"
 fi
   
 while [[ $# -gt 0 ]]
@@ -37,13 +32,8 @@ do
 key="$1"
 
 case $key in
-    -s|--skbuildplatform)
-    skbuild_platform="$2"
-    shift # past argument
-    shift # past value
-    ;;
     -p|--python)
-    python_executable="$2"
+    python_executables+=("$2")
     shift # past argument
     shift # past value
     ;;
@@ -81,44 +71,51 @@ if [[ "$openvds_version" == "" ]]; then
   echo "Please specify OpenVDS version using -v --version"
   exit 1
 fi
-
+if [[ "${python_executables[@]}" == "" ]];  then
+  echo "Please specify at least one python executable"
+  exit 1
+fi
 if [[ "$distribution" == "" ]]; then
     distribution="$platform_name"
 fi
 
-python_ver=$("$python_executable" -c "import sys;  print('.'.join(map(str, sys.version_info[:2])))")
-python_root_dir=$("$python_executable" -c "import sys; import os; print(os.path.dirname(sys.executable))")
+[[ -d $openvds_path/binpackage/$name-$openvds_version ]] || mkdir -p $openvds_path/binpackage/$name-$openvds_version
 
-openvds_path=$(realpath $openvds_path)
-
-if [[ "$skbuild_platform" == "" ]]; then
+for python_executable in "${python_executables[@]}"; do
+  python_ver=$("$python_executable" -c "import sys;  print('.'.join(map(str, sys.version_info[:2])))")
+  python_root_dir=$("$python_executable" -c "import sys; import os; print(os.path.dirname(sys.executable))")
+  
+  openvds_path=$(realpath $openvds_path)
+  
   skbuild_platform="$skplat_name-$python_ver"
-fi
+  
+  skbuild_dir="$openvds_path/_skbuild/$skbuild_platform"
+  build_dir="$openvds_path/_skbuild/internal_build_dir"
+  [[ -d $skbuild_dir ]] || mkdir -p $skbuild_dir
+  [[ -d $build_dir ]] || mkdir -p $build_dir
+  skbuild_dir=$(realpath $skbuild_dir)
+  build_dir=$(realpath $build_dir)
+  
+  cd "$build_dir"
+ 
+  echo "Do $python_executable to $skbuild_dir"
+  if [[ "$platform_name" == "win" ]]; then
+    cmake -DPython3_ROOT_DIR="$python_root_dir" -DCMAKE_INSTALL_PREFIX=$skbuild_dir/cmake-install $cmake_args -G"$cmake_generator" $toolset $openvds_path
+    cmake --build . --config Debug --target install
+    cmake --build . --config Release --target install
+  else
+    cmake -DPython3_ROOT_DIR="$python_root_dir" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$skbuild_dir/cmake-install $cmake_args -G"$cmake_generator" $toolset $openvds_path
+    cmake --build . --config Release --target install
+  fi
+ 
+  cd "$openvds_path"
+  [[ -d "$skbuild_dir/cmake-build" ]] || mkdir -p "$skbuild_dir/cmake-build"
+  cp -r "$build_dir"/* "$skbuild_dir/cmake-build"
+  
+  "$python_executable" setup.py --skip-cmake bdist_wheel 
+  cp -r $skbuild_dir/cmake-install/* binpackage/$name-$openvds_version
+done
 
-skbuild_dir="$openvds_path/_skbuild/$skbuild_platform"
-[[ -d $skbuild_dir ]] || mkdir -p $skbuild_dir
-skbuild_dir=$(realpath $skbuild_dir)
-
-cd "$skbuild_dir"
-
-[[ -d cmake-build ]] || mkdir cmake-build
-cd cmake-build
-
-if [[ "$platform_name" == "win" ]]; then
-  cmake -DPython3_ROOT_DIR="$python_root_dir" -DCMAKE_INSTALL_PREFIX=$skbuild_dir/cmake-install $cmake_args -G"$cmake_generator" $toolset $openvds_path
-  cmake --build . --config Debug --target install
-  cmake --build . --config Release --target install
-else
-  cmake -DPython3_ROOT_DIR="$python_root_dir" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$skbuild_dir/cmake-install $cmake_args -G"$cmake_generator" $toolset $openvds_path
-  cmake --build . --config Release --target install
-fi
-
-cd "$openvds_path"
-
-"$python_executable" setup.py --skip-cmake bdist_wheel 
-
-[[ -d binpackage ]] || mkdir binpackage
-cp -r $skbuild_dir/cmake-install binpackage/$name-$openvds_version
 cp dist/* binpackage/$name-$openvds_version/
 cd binpackage
 if [[ "$platform_name" == "win" ]]; then
