afb994ae6fe5592b488590f199ebac640375f419
Jørgen Lind <jorgen.lind@bluware.com>
2021-03-29 15:22:00 +0200

    Make sure python VolumeDataAccess test follows protocol

--- tests/python/VolumeDataAccessor.py
+++ tests/python/VolumeDataAccessor.py
@@ -8,8 +8,10 @@ if not TEST_URL:
 handle = openvds.open(TEST_URL, TEST_CONNECTION)
 acc = openvds.VolumeDataAccessManager(handle)
 r = acc.requestVolumeSubset((0,0,0),(100,100,100))
+r.waitForCompletion()
 
 pacc = acc.manager.createVolumeDataPageAccessor(acc.layout, openvds.DimensionsND.Dimensions_012, 0, 0, 256, acc.manager.AccessMode.AccessMode_ReadOnly)
 racc = acc.manager.create3DVolumeDataAccessorR32(pacc, -1000)
 v = racc.getValue((100,100,100))
 
+openvds.close(handle)
